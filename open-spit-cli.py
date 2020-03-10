dddcc524a2cac9947b4d02819d4c6096eeedad17
Sawyer Shepherd <contact@sawyershepherd.org>
2020-03-10 01:59:12 -0400

    Threading for server/client

--- open-spit-cli.py
+++ open-spit-cli.py
@@ -1,33 +1,54 @@
-import socket, os, random, tkinter, pygame, threadings
+import socket, os, random, sys
+from threading import Thread
 
-def h_s_ready():
+port = 8888
+
+def out_ready():
     input('Press enter to ready up')
+    print('You are ready!')
+    c.send(' '.encode())
 
-def h_c_ready():
+def in_ready():
     c.recv(8)
-    print('Client is ready!')
+    print('Opponent is ready!')
 
-print('Welcome to open-spit! ')
+out_ready_t = Thread(target = out_ready)
+in_ready_t = Thread(target = in_ready)
+
+def ready():
+    out_ready_t.start()
+    in_ready_t.start()
+    out_ready_t.join()
+    in_ready_t.join()
+
+print('Welcome to open-spit!')
 while True:
-    join_or_host = input('Would you like to join or host a game?')
-    if join_or_host == 'connect':
-        host = input('Enter the host\'s IP address:')
-        break
+    join_or_host = input('Would you like to join or host a game?\n>')
 
-    elif connect_or_host == 'host':
+    if join_or_host == 'join':
+        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            host = input('Enter the host\'s IP address:\n>')
+            try:
+                c.connect((host, port))
+                break
+            except:
+                print('Connection error')
+        print('Connected to ' + host)
+        ready()
+
+    elif join_or_host == 'host':
         host = socket.gethostname()
-        port = 8888
-        max_connections = 1
-        s = socket.socket()
-
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((host, port))
-        s.listen(max_connections)
-        print('Server running...')
+        s.listen(1)
+        print('Server running from ' + host + ' on port ' + str(port))
 
         while True:
             print('Waiting on a client...')
-
-            c, c_adr = s.accept()
-            print(c_adr + ' connected')
-
+            c, c_ip = s.accept()
+            print(str(c_ip[0]) + ' connected from port ' + str(c_ip[1]))
             #Thread and join the ready funcs
+            ready()
+    elif join_or_host == 'quit':
+        sys.exit()

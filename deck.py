9bcef0ff326a68390f1eb37d36b6784a2bcee457
Sawyer Shepherd <contact@sawyershepherd.org>
2020-03-11 03:49:20 -0400

    Protocol now UDP, adding game foundations

---
+++ deck.py
@@ -0,0 +1,11 @@
+colors = ['R', 'G', 'B', 'P']
+
+deck = {}
+for c in colors:
+    for n in range(1, 14):
+            deck.append(colors[c] = str(n))
+print(deck)
+
+print(deck)
+
+#fix it
--- open-spit-cli.py
+++ open-spit-cli.py
@@ -1,19 +1,8 @@
-import socket, os, random, sys
+import socket, random, _tkinter
+from tkinter import *
 from threading import Thread
-
-port = 8888
-
-def out_ready():
-    input('Press enter to ready up')
-    print('You are ready!')
-    c.send(' '.encode())
-
-def in_ready():
-    c.recv(8)
-    print('Opponent is ready!')
-
-out_ready_t = Thread(target = out_ready)
-in_ready_t = Thread(target = in_ready)
+from sys import exit
+from requests import get
 
 def ready():
     out_ready_t.start()
@@ -21,34 +10,86 @@ def ready():
     out_ready_t.join()
     in_ready_t.join()
 
-print('Welcome to open-spit!')
+def join_server():
+    title_screen_frame.pack_forget()
+    title_screen_frame.destroy()
+    join_screen_frame.pack()
+    host_ip_label.pack()
+    host_ip_entry.pack()
+    host = host_ip_entry.get()
+    enter_host_ip_button.pack()
+    back_button.pack()
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def connect():
+    host = host_ip_entry.get()
+    try:
+        c.connect((host, port))
+    except:
+        connection_error_label.pack()
+
+def host_server():
+    title_screen_frame.pack_forget()
+    title_screen_frame.destroy()
+    connection_frame.pack()
+    server_address_label.pack()
+    host_screen_frame.pack()
+    waiting_lable.pack()
+    back_button.pack()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('', port))
+
+'''
+    colors = ['R ', 'G ', 'B ', 'P ']
+    deck = []
+    for n in range(1, 14):
+        for c in colors:
+            deck.append(c + str(n))
+    random.shuffle(deck)
+'''
+
+def back():
+    main_frame.destroy()
+
 while True:
-    join_or_host = input('Would you like to join or host a game?\n>')
+    port = 8888
+    host = get('https://ipapi.co/ip').text
 
-    if join_or_host == 'join':
-        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        while True:
-            host = input('Enter the host\'s IP address:\n>')
-            try:
-                c.connect((host, port))
-                break
-            except:
-                print('Connection error')
-        print('Connected to ' + host)
-        ready()
+    root = Tk()
+    root.geometry('500x150')
+    root.title('Open-Spit')
+    main_frame = Frame(root)
 
-    elif join_or_host == 'host':
-        host = socket.gethostname()
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((host, port))
-        s.listen(1)
-        print('Server running from ' + host + ' on port ' + str(port))
+    back_button = Button(main_frame, text='Back', command = back)
 
-        while True:
-            print('Waiting on a client...')
-            c, c_ip = s.accept()
-            print(str(c_ip[0]) + ' connected from port ' + str(c_ip[1]))
-            #Thread and join the ready funcs
-            ready()
-    elif join_or_host == 'quit':
-        sys.exit()
+    title_screen_frame = Frame(main_frame)
+    #---------------------------------
+    welcome_label = Label(title_screen_frame, text='Welcome to Open-Spit!')
+    join_game_button = Button(title_screen_frame, text='Join Game', command=join_server)
+    host_game_button = Button(title_screen_frame, text='Host Game', command=host_server)
+
+    join_screen_frame = Frame(main_frame)
+    #-------------------------
+    host_ip_label = Label(join_screen_frame, text='Enter the host\'s IPv4 address')
+    host_ip_entry = Entry(join_screen_frame, text='test')
+    enter_host_ip_button = Button(join_screen_frame, text='Connect', command=connect)
+    connection_error_label = Label(join_screen_frame, text='Connection error, try again')
+
+    host_screen_frame = Frame(main_frame)
+    #--------------------------
+    waiting_lable = Label(host_screen_frame, text='Waiting on client...')
+
+    connection_frame = Frame(main_frame)
+    #----------------------------
+    server_address_label = Label(connection_frame, text='Server running at {} on port {}'.format(host, str(port)))
+
+
+    game_frame = Frame(main_frame)
+    #------------------
+
+    main_frame.pack()
+    title_screen_frame.pack()
+    welcome_label.pack()
+    join_game_button.pack()
+    host_game_button.pack()
+    input('')
